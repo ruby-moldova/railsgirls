e2ae689203599b53a332317ac4921ab2ff03b9e9
Andrei Istratii <andrei.istratii@gmail.com>
2015-04-09 12:04:00 +0300

    Refactor validation syntax.

--- app/models/candidate_profile.rb
+++ app/models/candidate_profile.rb
@@ -1,10 +1,5 @@
 class CandidateProfile < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
-  validates :city, presence: true
-  validates :birthday, presence: true
-  validates :activity, presence: true
-  validates :studies, presence: true
-  validates :motivation, presence: true
+  validates :name, :email, :phone, :city, :birthday,
+            :activity, :studies, :motivation,
+            :presence => true
 end
